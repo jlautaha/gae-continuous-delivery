78e14bd7fc4f9b93e2c5e65165c6601b7baa3119
Jon Lautaha <37350450+jlautaha@users.noreply.github.com>
2021-02-08 20:26:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 from flask import Flask
 from flask import jsonify
+import random
 app = Flask(__name__)
 
 @app.route('/')
@@ -15,6 +16,15 @@ def echo(name):
     val = {"new-name": name}
     return jsonify(val)
 
+@app.route('/coinflip/<choice>')
+def coinflip(choice)
+    x = random.choice(['heads','tails'])
+    if choice.lower() != 'heads' and choice.lower() != 'tails':
+        return 'Not a valid choice.'
+    if choice.lower() == x:
+        return f'You chose {choice} and you won!'
+    else:
+        return f'You chose {choice} and you lost!'
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
