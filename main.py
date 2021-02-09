9b3989d544805f6e81ef07f6c1174e967168a3b2
Jon Lautaha <37350450+jlautaha@users.noreply.github.com>
2021-02-08 20:47:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,5 +9,15 @@ def hello():
     print("I am inside hello world")
     return 'Hello World! CD'
 
+@app.route('/coinflip/<choice>')
+def coinflip(choice):
+    x = random.choice(['heads','tails'])
+    if choice.lower() != 'heads' and choice.lower() != 'tails':
+        return 'Not a valid choice.'
+    if choice.lower() == x:
+        return f'You chose {choice} and you won!'
+    else:
+        return f'You chose {choice} and you lost!'
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
